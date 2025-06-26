f5c5d0a88d5b71047a397e92853df62280590630
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-06-26 11:12:17 +0900

    refactor:엔티티 필드 추가 #back-6

--- src/main/java/com/sesac7/hellopet/global/Board.java
+++ src/main/java/com/sesac7/hellopet/global/Board.java
@@ -1,4 +1,15 @@
 package com.sesac7.hellopet.global;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Board {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
 }
