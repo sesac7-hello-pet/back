010b248c315c4b3826a9001e569d9fa748e7981b
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 19:04:30 +0900

    feat: Jwt토큰 생성을 위한 JwtUtil 작성[#-back-9]

---
+++ src/main/java/com/sesac7/hellopet/common/utils/JwtUtil.java
@@ -0,0 +1,62 @@
+package com.sesac7.hellopet.common.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.security}")
+    private String secret;
+
+    @Value("${jwt.access-token-expiration-ms}")
+    private Long expiration;
+
+    @Value("${jwt.refresh-token-expiration-ms}")
+    private Long refreshExpiration;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = secret.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public ResponseCookie generateCookie(UserDetails userDetails) {
+        String token = doGenerateToken(userDetails);
+        return ResponseCookie.from("accessToken", token)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofMinutes(expiration))
+                .sameSite("Strict")
+                .build();
+    }
+
+    public ResponseCookie generateRefreshCookie(UserDetails userDetails) {
+        String token = doGenerateToken(userDetails);
+        return ResponseCookie.from("refreshToken", token)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(Duration.ofMinutes(refreshExpiration))
+                .sameSite("Strict")
+                .build();
+    }
+
+    private String doGenerateToken(UserDetails userDetails) {
+        Claims claims = Jwts.claims();
+        claims.setSubject(userDetails.getUsername());
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+}
