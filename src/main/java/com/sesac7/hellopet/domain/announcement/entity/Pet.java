63c91419fd0bf818f913585b811bd1b2a4e59592
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 12:49:53 +0900

    feat: Pet Entity 생성[#-back-3]

---
+++ src/main/java/com/sesac7/hellopet/domain/announcement/entity/Pet.java
@@ -0,0 +1,35 @@
+package com.sesac7.hellopet.domain.announcement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "pets")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String health;
+
+    @Column(nullable = false)
+    private String personality;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false)
+    private String foundPlace;
+}
