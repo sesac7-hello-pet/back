ab96a2ebd5167e339a5816a79b67d894df26e00f
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 19:03:40 +0900

    feat: UserDetails를 구현하고 loadUserByUsername 재정의[#-back-9]

---
+++ src/main/java/com/sesac7/hellopet/domain/user/Service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.sesac7.hellopet.domain.user.Service;
+
+import com.sesac7.hellopet.domain.user.entity.User;
+import com.sesac7.hellopet.domain.user.repository.UserRepository;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail((email))
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 Email 입니다. "));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+    }
+}
