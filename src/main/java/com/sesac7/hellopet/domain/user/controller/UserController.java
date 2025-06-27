0c29dd50b97837865770be245865159c970a9b23
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 14:24:30 +0900

    feat: 유저데이터를 UserController에서 받기[#-back-7]

--- src/main/java/com/sesac7/hellopet/domain/user/controller/UserController.java
+++ src/main/java/com/sesac7/hellopet/domain/user/controller/UserController.java
@@ -2,8 +2,10 @@ package com.sesac7.hellopet.domain.user.controller;
 
 import com.sesac7.hellopet.domain.user.dto.request.UserRegisterRequest;
 import com.sesac7.hellopet.domain.user.dto.response.UserRegisterResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @PostMapping("/signup")
-    public ResponseEntity<UserRegisterResponse> register(UserRegisterRequest request) {
+    public ResponseEntity<UserRegisterResponse> register(@Valid @RequestBody UserRegisterRequest request) {
         return null;
     }
 }
