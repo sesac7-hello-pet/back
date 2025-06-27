a25f115b35c9d1f0ec60ba54b11b1032a70b6d5c
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 14:05:38 +0900

    feat: UserRegisterRequest, UserRegisterResponse 생성[#-back-7]

---
+++ src/main/java/com/sesac7/hellopet/domain/user/dto/request/UserRegisterRequest.java
@@ -0,0 +1,4 @@
+package com.sesac7.hellopet.domain.user.dto.request;
+
+public class UserRegisterRequest {
+}
---
+++ src/main/java/com/sesac7/hellopet/domain/user/dto/response/UserRegisterResponse.java
@@ -0,0 +1,4 @@
+package com.sesac7.hellopet.domain.user.dto.response;
+
+public class UserRegisterResponse {
+}
