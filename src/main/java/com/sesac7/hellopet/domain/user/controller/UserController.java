5d33db6ad253d18777b852a29d77fcaf1893088b
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 14:17:41 +0900

    feat: UserController에서 유저데이터 받기[#-back-7]

--- src/main/java/com/sesac7/hellopet/domain/user/controller/UserController.java
+++ src/main/java/com/sesac7/hellopet/domain/user/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.sesac7.hellopet.domain.user.controller;
 
+import com.sesac7.hellopet.domain.user.dto.request.UserRegisterRequest;
+import com.sesac7.hellopet.domain.user.dto.response.UserRegisterResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserRegisterResponse> register(UserRegisterRequest request) {
+        return null;
+    }
 }
--- src/main/java/com/sesac7/hellopet/domain/user/dto/request/UserRegisterRequest.java
+++ src/main/java/com/sesac7/hellopet/domain/user/dto/request/UserRegisterRequest.java
@@ -1,4 +1,14 @@
 package com.sesac7.hellopet.domain.user.dto.request;
 
+import com.sesac7.hellopet.domain.user.entity.UserRole;
+
 public class UserRegisterRequest {
+    private String email;
+    private String password;
+    private UserRole role;
+    private String nickname;
+    private String username;
+    private String address;
+    private Integer userPhoneNumber;
+    private String userProfileUrl;
 }
