e04c562e4f48ce38f0a64d5c157f0faec65893ce
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 15:38:36 +0900

    feat: AuthController, AuthService 생성[#-back-9]

---
+++ src/main/java/com/sesac7/hellopet/domain/auth/AuthService/AuthService.java
@@ -0,0 +1,11 @@
+package com.sesac7.hellopet.domain.auth.AuthService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthService {
+}
