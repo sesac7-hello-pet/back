eb1e74437f16cf35a8066af6af9d20650cdefb42
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-06-27 14:50:22 +0900

    feat : Board Repository /Board Controller / Board Service 생성 [#-back-6]

---
+++ src/main/java/com/sesac7/hellopet/domain/InformalBoard/controller/BoardController.java
@@ -0,0 +1,11 @@
+package com.sesac7.hellopet.domain.informalBoard.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
+public class InformalController {
+    private final
+
+}
---
+++ src/main/java/com/sesac7/hellopet/domain/InformalBoard/repository/BoardRepository.java
@@ -0,0 +1,4 @@
+package com.sesac7.hellopet.domain.informalBoard.repository;
+
+public class BoardRepository {
+}
