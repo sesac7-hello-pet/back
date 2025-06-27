97aa00ad2d7a37974e5b328261f2016360c6c1cd
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 12:56:35 +0900

    feat: BoardComment Entity 생성[#-back-3]

---
+++ src/main/java/com/sesac7/hellopet/domain/InformalBoard/entity/BoardComment.java
@@ -0,0 +1,38 @@
+package com.sesac7.hellopet.domain.InformalBoard.entity;
+
+import com.sesac7.hellopet.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "board_comments")
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String content;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private InformalBoard informalBoard;
+
+    @ManyToOne
+    private BoardComment parentComment;
+}
