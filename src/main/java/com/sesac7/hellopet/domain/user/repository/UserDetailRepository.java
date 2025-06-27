d55f06f6b25c834a73c630a0734fa230a5316fd1
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-27 15:18:06 +0900

    feat: UserDetailRepository 생성[#-back-7]

---
+++ src/main/java/com/sesac7/hellopet/domain/user/repository/UserDetailRepository.java
@@ -0,0 +1,9 @@
+package com.sesac7.hellopet.domain.user.repository;
+
+import com.sesac7.hellopet.domain.user.entity.UserDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
+}
