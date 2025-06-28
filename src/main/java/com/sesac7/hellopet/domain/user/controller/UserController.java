0d70eb88e438102e6e8af37d173956268b67ece8
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-29 01:38:13 +0900

    feat: 중복확인 엔드포인트 및 파라미터 생성[#back-12]

--- src/main/java/com/sesac7/hellopet/domain/user/controller/UserController.java
+++ src/main/java/com/sesac7/hellopet/domain/user/controller/UserController.java
@@ -1,16 +1,14 @@
 package com.sesac7.hellopet.domain.user.controller;
 
 import com.sesac7.hellopet.domain.user.Service.UserService;
+import com.sesac7.hellopet.domain.user.dto.request.CheckField;
 import com.sesac7.hellopet.domain.user.dto.request.UserRegisterRequest;
 import com.sesac7.hellopet.domain.user.dto.response.UserRegisterResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -22,5 +20,11 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<UserRegisterResponse> register(@Valid @RequestBody UserRegisterRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.userRegister(request));
+    }
+
+    @GetMapping("/exist")
+    public ResponseEntity<ExistResponse> check(@RequestParam CheckField field,
+                                               @RequestParam String value) {
+        return null;
     }
 }
