c344ba6ef07544d435495c64c6127437b3ecc62e
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-29 01:12:05 +0900

    feat: 데이터를 만들 메서드 클래스 생성[#back-13]

---
+++ src/main/java/com/sesac7/hellopet/common/utils/DataSetterImpl.java
@@ -0,0 +1,11 @@
+package com.sesac7.hellopet.common.utils;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+public class DataSetterImpl{
+}
