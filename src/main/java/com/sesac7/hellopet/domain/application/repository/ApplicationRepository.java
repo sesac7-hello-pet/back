c3992de8de5967dbc3aa030455324c5ebd9f081a
xeimun <hinote444@naver.com>
2025-07-01 02:01:38 +0900

    feat: 사용자 ID와 일치하는 신청서를 생성일 기준 내림차순으로 조회하는 쿼리 메서드 구현 [#back-31]

--- src/main/java/com/sesac7/hellopet/domain/application/repository/ApplicationRepository.java
+++ src/main/java/com/sesac7/hellopet/domain/application/repository/ApplicationRepository.java
@@ -1,9 +1,12 @@
 package com.sesac7.hellopet.domain.application.repository;
 
 import com.sesac7.hellopet.domain.application.entity.Application;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    Page<Application> findByApplicantIdOrderBySubmittedAtDesc(Long applicantId, Pageable pageable);
 }
