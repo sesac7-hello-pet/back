5f63794f5819d50930861fe1348b6bad5ec6c62d
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-07-01 10:56:04 +0900

    refactor: 엔드포인트 수정

--- src/main/java/com/sesac7/hellopet/domain/board/controller/BoardController.java
+++ src/main/java/com/sesac7/hellopet/domain/board/controller/BoardController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/board")
+@RequestMapping("/boards")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
