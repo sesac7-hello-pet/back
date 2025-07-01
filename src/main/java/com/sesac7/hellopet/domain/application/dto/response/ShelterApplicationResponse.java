515a806d2a978dca73822e6ecbd9aa8c93da918f
xeimun <hinote444@naver.com>
2025-07-01 16:34:34 +0900

    refactor: 페이징 정보를 포함하는 요청/응답 DTO 생성 [#back-37]

--- src/main/java/com/sesac7/hellopet/domain/application/dto/response/ShelterApplicationResponse.java
+++ src/main/java/com/sesac7/hellopet/domain/application/dto/response/ShelterApplicationResponse.java
@@ -1,5 +1,8 @@
 package com.sesac7.hellopet.domain.application.dto.response;
 
+import com.sesac7.hellopet.domain.announcement.entity.Announcement;
+import com.sesac7.hellopet.domain.application.entity.Application;
+import com.sesac7.hellopet.domain.user.entity.UserDetail;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,4 +15,15 @@ public class ShelterApplicationResponse {
     private String userName;
     private String userPhoneNumber;
     private String userEmail;
+
+    public static ShelterApplicationResponse from(Announcement announcement, Application application) {
+        UserDetail userDetail = application.getApplicant().getUserDetail();
+        return ShelterApplicationResponse.builder()
+                                         .announcementId(announcement.getId())
+                                         .announcementCreatedAt(announcement.getCreateAt())
+                                         .userName(userDetail.getUsername())
+                                         .userPhoneNumber(userDetail.getPhoneNumber())
+                                         .userEmail(application.getApplicant().getEmail())
+                                         .build();
+    }
 }
