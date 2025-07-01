3ff6659996e562f20e9777ab607810b672c7a650
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-07-01 12:45:15 +0900

    feat : 댓글 생성 메서드 구현 [#back-26]

--- src/main/java/com/sesac7/hellopet/domain/comment/service/CommentService.java
+++ src/main/java/com/sesac7/hellopet/domain/comment/service/CommentService.java
@@ -1,19 +1,41 @@
 package com.sesac7.hellopet.domain.comment.service;
 
 import com.sesac7.hellopet.common.utils.CustomUserDetails;
+import com.sesac7.hellopet.domain.board.entity.Board;
 import com.sesac7.hellopet.domain.board.repository.BoardRepository;
 import com.sesac7.hellopet.domain.comment.dto.request.CommentCreateRequest;
 import com.sesac7.hellopet.domain.comment.dto.response.CommentResponse;
+import com.sesac7.hellopet.domain.comment.entity.Comment;
 import com.sesac7.hellopet.domain.comment.repository.CommentRepository;
+import com.sesac7.hellopet.domain.user.entity.User;
+import com.sesac7.hellopet.domain.user.service.UserFinder;
+import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
     private CommentRepository commentRepository;
     private BoardRepository boardRepository;
+    private UserFinder userFinder;
 
     public CommentResponse createComment(CommentCreateRequest request, Long boardId,
                                          CustomUserDetails details) {
+
+        // 보드 게시글 
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new EntityNotFoundException("해당 게시글이 없습니다."));
+        // 유저 정보
+        User user = userFinder.findLoggedInUserByUsername(details.getUsername());
+        
+        Comment comment = Comment.builder()
+                .content(request.getContent())
+                .createdAt(LocalDateTime.now())
+                .board(board)
+                .user(user)
+                .build();
+
+        Comment saved = commentRepository.save(comment);
+        return CommentResponse.from(saved);
 
     }
 }
