07635203e89c12fab2fc271ac2ada2dcdad46487
xeimun <hinote444@naver.com>
2025-07-02 15:56:05 +0900

    refactor: 승인 응답용 DTO 필드 수정 [#back-44]

--- src/main/java/com/sesac7/hellopet/domain/application/dto/response/ApplicationApprovalResponse.java
+++ src/main/java/com/sesac7/hellopet/domain/application/dto/response/ApplicationApprovalResponse.java
@@ -1,6 +1,5 @@
 package com.sesac7.hellopet.domain.application.dto.response;
 
-import com.sesac7.hellopet.domain.application.entity.ApplicationStatus;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,15 +9,13 @@ import lombok.Getter;
 public class ApplicationApprovalResponse {
     private Long announcementId;
     private Long applicationId;
-    private ApplicationStatus status;
     private LocalDateTime processedAt;
     private String message;
 
-    public static ApplicationApprovalResponse of(Long announcementId, Long applicationId, ApplicationStatus status) {
+    public static ApplicationApprovalResponse of(Long announcementId, Long applicationId) {
         return ApplicationApprovalResponse.builder()
                                           .announcementId(announcementId)
                                           .applicationId(applicationId)
-                                          .status(status)
                                           .processedAt(LocalDateTime.now())
                                           .message("승인이 완료되었습니다.")
                                           .build();
