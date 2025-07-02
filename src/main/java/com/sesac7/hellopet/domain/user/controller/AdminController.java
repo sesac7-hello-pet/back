b514e805520c810e93ef4b8b04943ed8ed8c4758
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-02 22:04:49 +0900

    feat: userId를 변수로 수신[#back-47]

---
+++ src/main/java/com/sesac7/hellopet/domain/user/controller/AdminController.java
@@ -0,0 +1,21 @@
+package com.sesac7.hellopet.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@PreAuthorize("hasRole('ADMIN')")
+@RestController
+@RequestMapping("/admin/users")
+@RequiredArgsConstructor
+public class AdminController {
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deactivateUser(@PathVariable Long userId) {
+        
+    }
+}
