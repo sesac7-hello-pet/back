a5d42497aa27fcf53c9bb1d35087a557adf2649a
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-03 17:30:33 +0900

    feat: UnauthorizedException 에 삭제 쿠키 필드 추가[#back-17]

--- src/main/java/com/sesac7/hellopet/global/exception/custom/UnauthorizedException.java
+++ src/main/java/com/sesac7/hellopet/global/exception/custom/UnauthorizedException.java
@@ -1,7 +1,17 @@
 package com.sesac7.hellopet.global.exception.custom;
 
+import lombok.Getter;
+import org.springframework.http.ResponseCookie;
+
+@Getter
 public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException() {
+
+    private final ResponseCookie accessCookie;
+    private final ResponseCookie refreshCookie;
+
+    public UnauthorizedException(ResponseCookie accessCookie, ResponseCookie refreshCookie) {
         super("Refresh 토큰이 없습니다.");
+        this.accessCookie = accessCookie;
+        this.refreshCookie = refreshCookie;
     }
 }
