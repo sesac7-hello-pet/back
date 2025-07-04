29c3fce5efa425827cf9bbbaff0988616c367a53
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-04 11:50:57 +0900

    feat: filter 로직 변경[#back-61]

--- src/main/java/com/sesac7/hellopet/global/filter/JwtFilter.java
+++ src/main/java/com/sesac7/hellopet/global/filter/JwtFilter.java
@@ -4,21 +4,27 @@ import com.sesac7.hellopet.common.utils.CustomUserDetails;
 import com.sesac7.hellopet.common.utils.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.List;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+
+    private static final List<String> SKIP = List.of(
+            "/auth/**",
+            "/users/**"
+    );
+    private final AntPathMatcher matcher = new AntPathMatcher();
 
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
@@ -32,26 +38,29 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String token = Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
-                             .filter(c -> "accessToken".equals(c.getName()))
-                             .map(Cookie::getValue)
-                             .findFirst().orElse(null);
+        String access = jwtUtil.findCookie(request, "accessToken");
+        String refresh = jwtUtil.findCookie(request, "refreshToken");
 
-        if (token == null || jwtUtil.isTokenExpired(token)) {
+        if (access != null && !jwtUtil.isTokenExpired(access)) {
+            String email = jwtUtil.getEmailFromToken(access);
+
+            if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                CustomUserDetails ud = (CustomUserDetails) userDetailsService.loadUserByUsername(email);
+
+                if (jwtUtil.validateToken(access, ud)) {
+                    UsernamePasswordAuthenticationToken auth =
+                            new UsernamePasswordAuthenticationToken(ud, null, ud.getAuthorities());
+                    auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }
+
             filterChain.doFilter(request, response);
             return;
         }
 
-        String email = jwtUtil.getEmailFromToken(token);
-        if (SecurityContextHolder.getContext().getAuthentication() == null) {
-            CustomUserDetails ud = (CustomUserDetails) userDetailsService.loadUserByUsername(email);
-
-            if (jwtUtil.validateToken(token, ud)) {
-                UsernamePasswordAuthenticationToken auth =
-                        new UsernamePasswordAuthenticationToken(ud, null, ud.getAuthorities());
-                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            }
+        if ((access == null || jwtUtil.isTokenExpired(access)) && (refresh != null)) {
+            throw new CredentialsExpiredException("ACCESS TOKEN이 만료되었습니다.");
         }
 
         filterChain.doFilter(request, response);
