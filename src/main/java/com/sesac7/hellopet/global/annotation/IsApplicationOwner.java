a6f56b72b26b88c3f84c4869b3c9203c442b8d0c
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-04 17:17:43 +0900

    feat: 생성된 매서드를 사용할 커스텀 어노테이션 생성[#back-55]

---
+++ src/main/java/com/sesac7/hellopet/global/annotation/IsApplicationOwner.java
@@ -0,0 +1,13 @@
+package com.sesac7.hellopet.global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("@applicationSecurity.isOwner(#applicationId, authentication)")
+public @interface IsApplicationOwner {
+}
