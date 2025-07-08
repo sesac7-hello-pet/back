95543f5e86b6b54eee0799304aa8efd16db89718
xeimun <hinote444@naver.com>
2025-07-08 22:12:18 +0900

    refactor: 신청서 상태 거절 처리 로직 @Modifying 쿼리로 리팩토링 [#back-77]

--- src/main/java/com/sesac7/hellopet/domain/application/repository/ApplicationRepository.java
+++ src/main/java/com/sesac7/hellopet/domain/application/repository/ApplicationRepository.java
@@ -2,15 +2,16 @@ package com.sesac7.hellopet.domain.application.repository;
 
 import com.sesac7.hellopet.domain.application.entity.Application;
 import com.sesac7.hellopet.domain.application.entity.ApplicationStatus;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
@@ -20,13 +21,17 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
     @EntityGraph(attributePaths = {"applicant", "applicant.userDetail"})
     Page<Application> findByAnnouncementId(Long announcementId, Pageable pageable);
 
+    @Transactional
+    @Modifying(clearAutomatically = true)
     @Query("""
-                SELECT app FROM Application app
-                WHERE app.announcement.id = :announcementId
-                AND app.id != :applicationId
+                UPDATE Application a
+                SET a.status = 'REJECTED'
+                WHERE a.announcement.id = :announcementId
+                AND a.id != :applicationId
+                AND a.status = 'PENDING'
             """)
-    List<Application> findByAnnouncementIdAndExcludeApplicationId(@Param("announcementId") Long announcementId,
-                                                                  @Param("applicationId") Long applicationId);
+    int bulkRejectApplications(@Param("announcementId") Long announcementId,
+                               @Param("applicationId") Long applicationId);
 
     Optional<Application> findByIdAndAnnouncementIdAndStatus(Long applicationId, Long announcementId,
                                                              ApplicationStatus status);
